f580921d3588438ebb527f16625c6fc9d2042771
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-07-10 22:38:25 -0400

    Implemented a main menu which will be shown when the app is started

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const db = require("./db/connection");
+const inquirer = require("inquirer");
+const Menu = require("./lib/Menu")
 
 // Establish DB connection
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Database connected.");
-});
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log("Database connected.");
+// });
+
+const  init = async () => {
+  try {
+    // Get an option from a user
+    const selectedOption = await inquirer.prompt(Menu.mainMenu());
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+init();
